01aa98aac1c628c88690777020b83f0629d6736d
jaburtog <jaburto@casapellas.com>
2024-08-31 11:33:00 -0600

    sencond

--- Lab05_Maven1/src/main/java/package1/Hello_World.java
+++ Lab05_Maven1/src/main/java/package1/Hello_World.java
@@ -5,7 +5,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, World! 111");
+		System.out.println("Hello, World! 1111");
 		System.out.println("Hello, World! 2222");
 
         
