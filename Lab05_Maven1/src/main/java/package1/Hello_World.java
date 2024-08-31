7b871de2372c96d8554754a0ef72c04c3f7849cb
jaburtog <jaburto@casapellas.com>
2024-08-31 11:19:02 -0600

    First

---
+++ Lab05_Maven1/src/main/java/package1/Hello_World.java
@@ -0,0 +1,25 @@
+package package1;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World! 1");
+        
+        String hello = "A";
+        switch (hello) {
+            case "A":
+                System.out.println("one");
+                break;
+            case "B":
+                System.out.println("two");
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }
+
+	}
+
+}
